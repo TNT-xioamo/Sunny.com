e0719dd6c6e9471f23aa6ede63a8e88f6cd45315
TNT-xiaomo <1057731726@qq.com>
2021-08-27 18:20:47 +0800

    背景连线

--- themes/fluid/source/js/special_efficacy.js
+++ themes/fluid/source/js/special_efficacy.js
@@ -1,7 +1,13 @@
 (function (window, document) {
+  // 添加鼠标特效
   const scriptTag = document.createElement('script')
   scriptTag.type = 'text/javascript'
   scriptTag.src = '/js/fireworks.js'
   // scriptTag.src = 'https://blog-static.cnblogs.com/files/zouwangblog/mouse-click.js'
   document.getElementsByTagName("body")[0].appendChild(scriptTag)
+  // 添加canvas背景
+  const scriptBg = document.createElement('script')
+  scriptBg.type = 'text/javascript'
+  scriptBg.src = '/js/canvas-origin.js'
+  document.getElementsByTagName("main")[0].appendChild(scriptBg)
 })(window, document)
